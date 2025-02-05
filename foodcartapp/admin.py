0c7a0172f7e4535782f422408eff3bcd9e39edc7
Roman <frideriko26_93@mail.ru>
2025-02-05 14:22:38 +0300

    update price in orderitems and order

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -16,8 +16,6 @@ class RestaurantMenuItemInline(admin.TabularInline):
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
-    fields = ['product', 'quantity']
-
 
 
 @admin.register(Restaurant)
@@ -118,16 +116,6 @@ class OrderAdmin(admin.ModelAdmin):
         OrderItemInline
     ]
 
-    def save_formset(self, request, form, formset, change):
-        instances = formset.save(commit=False)
-        for obj in formset.deleted_objects:
-            obj.delete()
-        for instance in instances:
-            if not instance.product:
-                raise ValueError("Product must be specified")
-            instance.price = instance.product.price
-            instance.save()
-        formset.save_m2m()
 
 
 
